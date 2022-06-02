f0808ff8836d00c89dd88806cfcb67ba9c18f193
Muhammad Hassaan Ali <103775441+hassaanalii@users.noreply.github.com>
2022-06-02 21:19:49 +0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Development\\flutter\\tranquility" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter"
+  "PROJECT_DIR=D:\\Development\\flutter\\tranquility"
+  "FLUTTER_ROOT=D:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Development\\flutter\\tranquility\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Development\\flutter\\tranquility"
+  "FLUTTER_TARGET=D:\\Development\\flutter\\tranquility\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Development\\flutter\\tranquility\\.dart_tool\\package_config.json"
+)
